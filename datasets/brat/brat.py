568b02ae9050c08b99ad095e601ee88aa993ab78
Arne Binder <arne.b.binder@gmail.com>
2020-07-24 17:01:34 +0200

    implement full BRAT v1.3 specification

--- datasets/brat/brat.py
+++ datasets/brat/brat.py
@@ -17,38 +17,59 @@ class Brat(nlp.GeneratorBasedBuilder):
 
     def _info(self):
         return nlp.DatasetInfo(
-            features=nlp.Features(
-                {
-                    "context": nlp.Value("string"),
-                    "spans": nlp.Sequence({
-                        "id": nlp.Value("string"),
-                        "type": nlp.Value("string"),
-                        "locations": nlp.Sequence({
-                            "start": nlp.Value("int"),
-                            "end": nlp.Value("int"),
-                        }),
-                        "text": nlp.Value("string"),
+            features=nlp.Features({
+                "context": nlp.Value("string"),
+                "spans": nlp.Sequence({
+                    "id": nlp.Value("string"),
+                    "type": nlp.Value("string"),
+                    "locations": nlp.Sequence({
+                        "start": nlp.Value("int32"),
+                        "end": nlp.Value("int32"),
                     }),
-                    "relations": nlp.Sequence({
-                        "id": nlp.Value("string"),
+                    "text": nlp.Value("string"),
+                }),
+                "relations": nlp.Sequence({
+                    "id": nlp.Value("string"),
+                    "type": nlp.Value("string"),
+                    "arguments": nlp.Sequence({
                         "type": nlp.Value("string"),
-                        "arguments": nlp.Sequence({
-                            "type": nlp.Value("string"),
-                            "target": nlp.Value("string")
-                        })
-                    }),
-                    "events": nlp.Sequence({
-                        "id": nlp.Value("string"),
+                        "target": nlp.Value("string")
+                    })
+                }),
+                "equivalence_relations": nlp.Sequence({
+                    "type": nlp.Value("string"),
+                    "targets": nlp.Sequence(nlp.Value("string")),
+                }),
+                "events": nlp.Sequence({
+                    "id": nlp.Value("string"),
+                    "type": nlp.Value("string"),
+                    "trigger": nlp.Value("string"),
+                    "arguments": nlp.Sequence({
                         "type": nlp.Value("string"),
-                        "trigger": nlp.Value("string"),
-                        "arguments": nlp.Sequence({
-                            "type": nlp.Value("string"),
-                            "target": nlp.Value("string")
-                        })
-                    }),
-                    # TODO: add attributions
-                    # TODO: add normalizations
-                })
+                        "target": nlp.Value("string")
+                    })
+                }),
+                "attributions": nlp.Sequence({
+                    "id": nlp.Value("string"),
+                    "type": nlp.Value("string"),
+                    "target": nlp.Value("string"),
+                    "value": nlp.Value("string"),
+
+                }),
+                "normalizations": nlp.Sequence({
+                    "id": nlp.Value("string"),
+                    "type": nlp.Value("string"),
+                    "target": nlp.Value("string"),
+                    "resource_id": nlp.Value("string"),
+                    "entity_id": nlp.Value("string"),
+                }),
+                "notes": nlp.Sequence({
+                    "id": nlp.Value("string"),
+                    "type": nlp.Value("string"),
+                    "target": nlp.Value("string"),
+                    "note": nlp.Value("string"),
+                }),
+            })
         )
 
     def _split_generators(self, dl_manager):
@@ -81,64 +102,157 @@ class Brat(nlp.GeneratorBasedBuilder):
         return {'start': int(parts[0]), 'end': int(parts[1])}
 
     @staticmethod
-    def _get_span_annotation(annotation_text):
-        res = {}
-        _id, _remaining = annotation_text.split('\t', maxsplit=1)
-        res['id'] = _id
-        _annot, _text = _remaining.split('\t', maxsplit=1)
-        res['text'] = _text
-        _type, _locations = _annot.split(' ', maxsplit=1)
-        res['type'] = _type
-        res['locations'] = [Brat._get_location(loc) for loc in _locations.split(';')]
-        
-        return res
+    def _get_span_annotation(annotation_line):
+        """
+        example input:
+        T1	Organization 0 4	Sony
+        """
+
+        _id, remaining, text = annotation_line.split('\t', maxsplit=2)
+        _type, locations = remaining.split(' ', maxsplit=1)
+        return {
+            'id': _id,
+            'text': text,
+            'type': _type,
+            'locations': [Brat._get_location(loc) for loc in locations.split(';')]
+        }
 
     @staticmethod
-    def _get_event_annotation(annotation_text):
-        raise NotImplementedError('implement _get_event_annotation()!')
+    def _get_event_annotation(annotation_line):
+        """
+        example input:
+        E1	MERGE-ORG:T2 Org1:T1 Org2:T3
+        """
+        _id, remaining = annotation_line.strip().split('\t')
+        args = [dict(zip(['type', 'target'], a.split(':'))) for a in remaining.split(' ')]
+        return {
+            'id': _id,
+            'type': args[0]['type'],
+            'trigger': args[0]['target'],
+            'arguments': args[1:]
+        }
 
     @staticmethod
-    def _get_relation_annotation(annotation_text):
-        raise NotImplementedError('implement _get_relation_annotation()!')
+    def _get_relation_annotation(annotation_line):
+        """
+        example input:
+        R1	Origin Arg1:T3 Arg2:T4
+        """
+
+        _id, remaining = annotation_line.strip().split('\t')
+        _type, remaining = remaining.split(' ', maxsplit=1)
+        args = [dict(zip(['type', 'target'], a.split(':'))) for a in remaining.split(' ')]
+        return {
+            'id': _id,
+            'type': _type,
+            'arguments': args
+        }
 
     @staticmethod
-    def _get_attribute_annotation(annotation_text):
-        raise NotImplementedError('implement _get_attribute_annotation()!')
+    def _get_equivalence_relation_annotation(annotation_line):
+        """
+        example input:
+        *	Equiv T1 T2 T3
+        """
+        _, remaining = annotation_line.strip().split('\t')
+        parts = remaining.split(' ')
+        return {
+            'type': parts[0],
+            'targets': parts[1:]
+        }
 
     @staticmethod
-    def _get_normalization_annotation(annotation_text):
-        raise NotImplementedError('implement _get_normalization_annotation()!')
+    def _get_attribute_annotation(annotation_line):
+        """
+        example input (binary: implicit value is True, if present, False otherwise):
+        A1	Negation E1
+        example input (multi-value: explicit value)
+        A2	Confidence E2 L1
+        """
+
+        _id, remaining = annotation_line.strip().split('\t')
+        parts = remaining.split(' ')
+        # if no value is present, it is implicitly "true"
+        if len(parts) == 2:
+            parts.append('true')
+        return {
+            'id': _id,
+            'type': parts[0],
+            'target': parts[1],
+            'value': parts[2],
+        }
+
+    @staticmethod
+    def _get_normalization_annotation(annotation_line):
+        """
+        example input:
+        N1	Reference T1 Wikipedia:534366	Barack Obama
+        """
+        _id, remaining, text = annotation_line.split('\t', maxsplit=2)
+        _type, target, ref = remaining.split(' ')
+        res_id, ent_id = ref.split(':')
+        return {
+            'id': _id,
+            'type': _type,
+            'target': target,
+            'resource_id': res_id,
+            'entity_id': ent_id,
+        }
+
+    @staticmethod
+    def _get_note_annotation(annotation_line):
+        """
+        example input:
+        #1	AnnotatorNotes T1	this annotation is suspect
+        """
+        _id, remaining, note = annotation_line.split('\t', maxsplit=2)
+        _type, target = remaining.split(' ')
+        return {
+            'id': _id,
+            'type': _type,
+            'target': target,
+            'note': note,
+        }
 
     @staticmethod
     def _read_annotation_file(filename):
         """
-        reads a BRAT annotations file (see https://brat.nlplab.org/standoff.html)
+        reads a BRAT v1.3 annotations file (see https://brat.nlplab.org/standoff.html)
         """
 
         res = {
             'spans': [],
             'events': [],
             'relations': [],
+            'equivalence_relations': [],
             'attributions': [],
             'normalizations': [],
+            'notes': [],
         }
 
         with open(filename) as file:
             for line in file:
-                # remove leading whitespace
-                line = line.lstrip()
-                if len(line) == 0 or line[0] == '#':
+                if len(line) == 0:
                     continue
-                elif line[0] == 'T':
+                ann_type = line[0]
+
+                # strip away the new line character
+                line = line[:-1]
+
+                if ann_type == 'T':
                     res['spans'].append(Brat._get_span_annotation(line))
-                elif line[0] == 'E':
+                elif ann_type == 'E':
                     res['events'].append(Brat._get_event_annotation(line))
-                elif line[0] == 'R':
+                elif ann_type == 'R':
                     res['relations'].append(Brat._get_relation_annotation(line))
-                elif line[0] == 'A':
+                elif ann_type == '*':
+                    res['relations'].append(Brat._get_equivalence_relation_annotation(line))
+                elif ann_type in ['A', 'M']:
                     res['attributions'].append(Brat._get_attribute_annotation(line))
-                elif line[0] == 'N':
+                elif ann_type == 'N':
                     res['normalizations'].append(Brat._get_normalization_annotation(line))
+                elif ann_type == '#':
+                    res['notes'].append(Brat._get_note_annotation(line))
                 else:
                     raise ValueError(f'unknown BRAT id type: {line[0]}. Annotation ids have to start with T (spans), '
                                      f'E (events), R (relations), A (attributions), or N (normalizations). See '
@@ -148,7 +262,6 @@ class Brat(nlp.GeneratorBasedBuilder):
 
     def _generate_examples(self, files):
         """ Read files sequentially, then lines sequentially. """
-        #idx = 0
         for filename in files:
             basename = path.basename(filename)
 
